5162b0e962aa248da4e5c38529a9833662da5e1e
Trung Hieu <trunghieupham03@gmail.com>
2022-08-28 21:58:23 +0700

    CMake install

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/coding/c++/year2/PBL02
+# Build directory: D:/coding/c++/year2/PBL02/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

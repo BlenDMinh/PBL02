fc533409c9017020727835df8169d0f36c520775
Trung Hieu <trunghieupham03@gmail.com>
2022-08-31 07:23:54 +0700

    #

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -5,52 +5,13 @@
 
 MyApp::MyApp() {
     app_ = App::Create();
-
-    ///
-    /// Create a resizable window by passing by OR'ing our window flags with
-    /// kWindowFlags_Resizable.
-    ///
-    window_ = Window::Create(app_->main_monitor(), WINDOW_WIDTH, WINDOW_HEIGHT,
-        false, kWindowFlags_Titled | kWindowFlags_Resizable);
-
-    ///
-    /// Create our HTML overlay-- we don't care about its initial size and
-    /// position because it'll be calculated when we call OnResize() below.
-    ///
+    window_ = Window::Create(app_->main_monitor(), WINDOW_WIDTH, WINDOW_HEIGHT, false, kWindowFlags_Titled | kWindowFlags_Resizable);
     overlay_ = Overlay::Create(window_, 1, 1, 0, 0);
-
-    ///
-    /// Force a call to OnResize to perform size/layout of our overlay.
-    ///
     OnResize(window_.get(), window_->width(), window_->height());
-
-    ///
-    /// Load a page into our overlay's View
-    ///
     overlay_->view()->LoadURL("file:///app.html");
-
-    ///
-    /// Register our MyApp instance as an AppListener so we can handle the
-    /// App's OnUpdate event below.
-    ///
     app_->set_listener(this);
-
-    ///
-    /// Register our MyApp instance as a WindowListener so we can handle the
-    /// Window's OnResize event below.
-    ///
     window_->set_listener(this);
-
-    ///
-    /// Register our MyApp instance as a LoadListener so we can handle the
-    /// View's OnFinishLoading and OnDOMReady events below.
-    ///
     overlay_->view()->set_load_listener(this);
-
-    ///
-    /// Register our MyApp instance as a ViewListener so we can handle the
-    /// View's OnChangeCursor and OnChangeTitle events below.
-    ///
     overlay_->view()->set_view_listener(this);
 }
 
@@ -63,11 +24,8 @@ void MyApp::Run() {
 }
 
 void MyApp::OnUpdate() {
-    ///
     /// This is called repeatedly from the application's update loop.
-    ///
     /// You should update any app logic here.
-    ///
 }
 
 void MyApp::OnClose(ultralight::Window* window) {
@@ -75,88 +33,54 @@ void MyApp::OnClose(ultralight::Window* window) {
 }
 
 void MyApp::OnResize(ultralight::Window* window, uint32_t width, uint32_t height) {
-    ///
     /// This is called whenever the window changes size (values in pixels).
-    ///
     /// We resize our overlay here to take up the entire window.
-    ///
     overlay_->Resize(width, height);
 }
 
-void MyApp::OnFinishLoading(ultralight::View* caller,
-    uint64_t frame_id,
-    bool is_main_frame,
-    const String& url) {
-    ///
+void MyApp::OnFinishLoading(ultralight::View* caller, uint64_t frame_id, bool is_main_frame, const String& url) {
     /// This is called when a frame finishes loading on the page.
-    ///
 }
 
 JSValueRef ButtonClick(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception) {
     const char* str = 
     "document.getElementById('result').innerText = 'Ultralight rocks!'";
-  
     // Create our string of JavaScript
     JSStringRef script = JSStringCreateWithUTF8CString(str);
-
     // Execute it with JSEvaluateScript, ignoring other parameters for now
     JSEvaluateScript(ctx, script, 0, 0, 0, 0);
-
     // Release our string (we only Release what we Create)
     JSStringRelease(script);
     return JSValueMakeNull(ctx);
 }
 
-void MyApp::OnDOMReady(ultralight::View* caller,
-    uint64_t frame_id,
-    bool is_main_frame,
-    const String& url) {
+void MyApp::OnDOMReady(ultralight::View* caller, uint64_t frame_id, bool is_main_frame, const String& url) {
     // Acquire the JS execution context for the current page.
-    //
-    // This call will lock the execution context for the current
-    // thread as long as the Ref<> is alive.
-    //
+    // This call will lock the execution context for the current thread as long as the Ref<> is alive.
     RefPtr<JSContext> context = caller->LockJSContext();
-
     // Get the underlying JSContextRef for use with the
     // JavaScriptCore C API.
     JSContextRef ctx = context.get() -> ctx();
-
     // Create a JavaScript String containing the name of our callback.
     JSStringRef name = JSStringCreateWithUTF8CString("ButtonClick");
-
-    // Create a garbage-collected JavaScript function that is bound to our
-    // native C callback 'OnButtonClick()'.
-    JSObjectRef func = JSObjectMakeFunctionWithCallback(ctx, name, 
-                                                        ButtonClick);
-
+    // Create a garbage-collected JavaScript function that is bound to our native C callback 'OnButtonClick()'.
+    JSObjectRef func = JSObjectMakeFunctionWithCallback(ctx, name, ButtonClick);
     // Get the global JavaScript object (aka 'window')
     JSObjectRef globalObj = JSContextGetGlobalObject(ctx);
-
-    // Store our function in the page's global JavaScript object so that it
-    // accessible from the page as 'OnButtonClick()'.
+    // Store our function in the page's global JavaScript object so that it accessible from the page as 'OnButtonClick()'.
     JSObjectSetProperty(ctx, globalObj, name, func, 0, 0);
-
     // Release the JavaScript String we created earlier.
     JSStringRelease(name);
 }
 
-void MyApp::OnChangeCursor(ultralight::View* caller,
-    Cursor cursor) {
-    ///
+void MyApp::OnChangeCursor(ultralight::View* caller, Cursor cursor) {
     /// This is called whenever the page requests to change the cursor.
-    ///
     /// We update the main window's cursor here.
-    ///
     window_->SetCursor(cursor);
 }
 
-void MyApp::OnChangeTitle(ultralight::View* caller,
-    const String& title) {
-    ///
+void MyApp::OnChangeTitle(ultralight::View* caller, const String& title) {
     /// This is called whenever the page requests to change the title.
-    ///
     /// We update the main window's title here.
-    ///
     window_->SetTitle(title.utf8().data());
 }

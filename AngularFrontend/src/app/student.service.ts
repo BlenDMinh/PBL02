ed38a573ced3e3660e1d83f5b0a53ef7a3f6cfe2
hieupt <trunghieupham03@gmail.com>
2022-09-29 14:23:25 +0700

    #

--- AngularFrontend/src/app/student.service.ts
+++ AngularFrontend/src/app/student.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Student } from './student';
 
 const baseurl = 'http://localhost:8000/api/StudentManager';
 @Injectable({

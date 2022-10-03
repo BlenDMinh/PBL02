abd4dce1def298151e1ecc5a1d23c0555778827e
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2022-10-03 15:08:06 +0800

    Temporary working database module

--- Database/Database.py
+++ Database/Database.py
@@ -14,7 +14,8 @@ def create_connection(db_file):
 
 def Execute(SQLQuery):
     print(f"SQL Executing:\n{SQLQuery}")
-    cur = create_connection(settings.DEFAULT_DATABASE_PATH).cursor()
+    con = create_connection(settings.DEFAULT_DATABASE_PATH)
+    cur = con.cursor()
     try:
         cur.execute(SQLQuery)
         return cur
@@ -28,36 +29,39 @@ def GetAllProperties(TableName):
 
 def GetAllTables():
     cur = Execute("SELECT name FROM sqlite_master WHERE type='table';")
-    print(cur.fetchall())
-    return cur.fetchall()
+    return list(map(lambda k: k[0], cur.fetchall()))
 
 def InitTable(TableName):
+    print(f"Init table {TableName}")
     properties = settings.config[TableName]
     print(properties)
     # CREATE TABLE IF NOT EXIST
-    if TableName not in GetAllTables():
+    tables = list(GetAllTables())
+    print(tables)
+    if TableName not in tables:
         if(len(properties['properties']) == 0):
             Execute(f"CREATE TABLE IF NOT EXISTS {TableName} (Empty CHAR);")
         else:
             Execute(f"CREATE TABLE IF NOT EXISTS {TableName} ({', '.join(map(lambda k: k + ' ' + properties['properties'][k], properties['properties']))});")
             if 'PRIMARY KEY' in properties:
                 Execute(f"ALTER TABLE {TableName} ADD CONSTRAINT PK_{TableName} PRIMARY KEY ({', '.join(properties['PRIMARY KEY'])})")
-    
-    # MODIFY TABLE
-    table_properties = GetAllProperties(TableName)
-    print(table_properties)
-    if(len(properties['properties']) == 0):
-        if 'Empty' not in table_properties:
-            Execute(f"ALTER TABLE {TableName} ADD COLUMN Empty CHAR")
-        for property in table_properties:
-            if property != 'Empty':
-                Execute(f"ALTER TABLE {TableName} DROP COLUMN {property}")
+            else:
+                pass
     else:
-        for property in properties:
-            if property not in table_properties:
-                Execute(f"ALTER TABLE {TableName} ADD {property} {properties[property]}")
-        for property in table_properties:
-            if property not in properties:
-                Execute(f"ALTER TABLE {TableName} DROP COLUMN {property}")
-
-    pass
+        print('=' * 20 + f"MODIFY TABLE {TableName}" + "=" * 20)
+        # MODIFY TABLE
+        table_properties = GetAllProperties(TableName)
+        print(table_properties)
+        if(len(properties['properties']) == 0):
+            if 'Empty' not in table_properties:
+                Execute(f"ALTER TABLE {TableName} ADD COLUMN Empty CHAR")
+            for property in table_properties:
+                if property != 'Empty':
+                    Execute(f"ALTER TABLE {TableName} DROP COLUMN {property}")
+        else:
+            for property in properties['properties']:
+                if property not in table_properties:
+                    Execute(f"ALTER TABLE {TableName} ADD {property} {properties['properties'][property]}")
+            for property in table_properties:
+                if property not in properties['properties']:
+                    Execute(f"ALTER TABLE {TableName} DROP COLUMN {property}")

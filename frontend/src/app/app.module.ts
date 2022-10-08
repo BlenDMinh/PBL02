5bb6c870b7060a9ea052c96b6b601b62a6ebfde9
hieupt <trunghieupham03@gmail.com>
2022-10-08 08:46:56 +0700

    Temporary sidebar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StudentTableComponent } from './components/student-table/student-table.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, StudentTableComponent, StudentListComponent],
+  declarations: [AppComponent, StudentTableComponent, StudentListComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/components/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass'],
+})
+export class NavComponent implements OnInit {
+  active = 'top';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

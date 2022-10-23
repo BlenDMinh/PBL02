a96242085f6cac39fc6c1c89df56c1eef2531e37
hieupt <trunghieupham03@gmail.com>
2022-10-23 15:36:14 +0700

    #

---
+++ frontend/src/app/models/class.ts
@@ -0,0 +1,7 @@
+export interface Class {
+  sectionID: number;
+  subjectID: number;
+  teacherID: number;
+  time: string;
+  capacity: number;
+}
---
+++ frontend/src/app/models/subject.ts
@@ -0,0 +1,4 @@
+export interface Subject {
+  subjectID: number;
+  subjectName: string;
+}

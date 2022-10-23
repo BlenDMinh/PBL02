1b021978752ca2d50426da44ce9c8f59971b7803
hieupt <trunghieupham03@gmail.com>
2022-10-23 15:43:49 +0700

    #

--- frontend/src/app/components/login-box/login-box.component.ts
+++ frontend/src/app/components/login-box/login-box.component.ts
@@ -1,35 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
-import { FormBuilder , FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-box',
   templateUrl: './login-box.component.html',
-  styleUrls: ['./login-box.component.scss']
+  styleUrls: ['./login-box.component.scss'],
 })
 export class LoginBoxComponent implements OnInit {
-  loginForm:any;
-  error:String = '';
-  constructor(private formBuilder: FormBuilder, private loginService: LoginService) {
-    
-  }
+  loginForm: any;
+  error: String = '';
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      id:['', Validators.required],
-      password:['', Validators.required]
+      id: ['', Validators.required],
+      password: ['', Validators.required],
     });
   }
   Login() {
-    if(this.loginForm.dirty && this.loginForm.valid) {
+    if (this.loginForm.dirty && this.loginForm.valid) {
       this.loginService
         .getToken(this.loginForm.value.id, this.loginForm.value.password)
         .subscribe((data) => {
-          if(data.hasOwnProperty('token')) {
-            localStorage.setItem('token', data['token'])
+          if (data.hasOwnProperty('token')) {
+            localStorage.setItem('token', data['token']);
             window.location.href = '/';
           } else {
-            this.error = data['error']
+            this.error = data['error'];
           }
         });
     }

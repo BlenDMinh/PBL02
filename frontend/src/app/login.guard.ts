19f8bd49dc354055a0012d1e0b9761b87d2ca074
hieupt <trunghieupham03@gmail.com>
2022-10-26 19:43:02 +0700

    #

--- frontend/src/app/login.guard.ts
+++ frontend/src/app/login.guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { LoginService } from './services/login.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginGuard implements CanActivate {
-  constructor(private login: LoginService, private router: Router) {}
+  constructor(private login: LoginService) {}
 
   canActivate(): boolean {
     if (this.login.loginByToken()) return true;
     else {
-      this.router.navigate(['/main']);
+      location.replace('/main');
       return false;
     }
   }

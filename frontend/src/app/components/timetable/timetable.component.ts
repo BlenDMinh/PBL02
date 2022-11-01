22c22edd468c74d1d867c44dce46571b4d3519bb
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2022-10-31 22:52:37 -0700

    #

--- frontend/src/app/components/timetable/timetable.component.ts
+++ frontend/src/app/components/timetable/timetable.component.ts
@@ -7,12 +7,18 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./timetable.component.scss'],
 })
 export class TimetableComponent implements OnInit {
+<<<<<<< Updated upstream
   loginUser: any = null;
+=======
+  table: [string, number][][] = [];
+  loginUser: any;
+>>>>>>> Stashed changes
   link = 'timetable';
 
   constructor(private loginService: LoginService) {}
 
   ngOnInit(): void {
+    // this.table[0][0] = ['a', 0]
     this.loginService.loginByToken().subscribe((student) => {
       this.loginUser = student;
       if (this.loginUser.hasOwnProperty('error')) {

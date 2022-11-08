9c71d07cbf2a9feb8687c6f2fe26d3c03126d630
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2022-11-08 13:44:24 +0700

    #

--- Database/TeacherDatabase.py
+++ Database/TeacherDatabase.py
@@ -4,12 +4,12 @@ from Database.IDatabase import IDatabase
 class TeacherDatabase(IDatabase):
     @staticmethod
     def GetAll():
-        cur = Database.Execute("SELECT * FROM Teacher")
+        cur = Database.Execute("SELECT * FROM Teacher", Debug=False)
         return cur.fetchall()
 
     @staticmethod
     def Get(pk):
-        cur = Database.Execute(f"SELECT * FROM Teacher WHERE TeacherID='{pk}'")
+        cur = Database.Execute(f"SELECT * FROM Teacher WHERE TeacherID='{pk}'", Debug=False)
         return cur.fetchone()
     
     @staticmethod
